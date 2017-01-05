7095ddf89fbeeddebf7f2b0b954089313de1b1da
Brycen Dorsay <dorsay@live.ca>
2017-01-05 15:18:17 -0800

    looks good and it works

--- actions.js
+++ actions.js
@@ -1,36 +1,61 @@
-$(document).ready(function() {
+var base_url = 'http://localhost:9000/';
+
+$(function() {
 
   var color = '';
   var inverted = '';
-
-  var colors = [
-    'EF5350',
-    'EC407A',
-    'AB47BC',
-    '7E57C2',
-    '5C6BC0',
-    '42A5F5',
-    '29B6F6',
-    '26C6DA',
-    '26A69A',
-    '66BB6A',
-    '9CCC65',
-    'D4E157',
-    'FFEE58',
-    'FFCA28',
-    'FFA726',
-    'FF7043',
-    '8D6E63',
-    'BDBDBD',
-    '78909C'
-  ];
+  var mode = 'shorten';
+  
 
   color += colors[Math.floor(Math.random() * colors.length)];
   inverted += invertHex(color);
 
   $('body').css({'background': '#' + color});
   $("input[type='submit']").css({'background-color': '#' + inverted});
-  $("input[type='text']").css({'outline-color': '#' + inverted});
+  $("input[type='text']").focus(function() {
+    $("input[type='text']").css({'box-shadow': 'inset 0 0 1px 2px #' + inverted});
+  });
+  $("input[type='text']").blur(function() {
+    $("input[type='text']").css({'box-shadow': ''});
+  });
+
+  $('#description').text(descriptions[Math.floor(Math.random() * descriptions.length)]);
+
+  $("input[type='submit']").click(function() {
+    if(mode == 'shorten' && $('#addressField').val() != '') {
+      mode = 'again';
+      send();
+      $("input[type='submit']").css({'width': '15%', 'border-radius': '5px 0 0 5px'}).val('AGAIN');
+      $("input[type='text']").css({'width': '0'}).val('');
+      $('#code').css({'width': '85%'});
+    } else if(mode == 'shorten' && $('#addressField').val() == '') {
+      $("input[type='text']").focus();
+    } else {
+      mode = 'shorten';
+      $("input[type='submit']").css({'width': '15%', 'border-radius': '0 5px 5px 0'}).val('SHORTN');
+      $("input[type='text']").css({'width': '85%'});
+      $('#notify').css({'width': '0'});
+      $('#code').css({'width': '0'}).text('');
+    }
+  });
+
+  $("input[type='text']").keypress(function(e) {
+    if (e.which == 13) {
+      $("input[type='submit']").click();
+    }
+  });
+
+  $('#code').click(function() {
+    $('#notify').css({'width': '15%', 'background-color': '#' + inverted});
+    $('#code').css({'width': '70%', 'border-radius': '0'});
+  });
+
+  new Clipboard('#code', {
+    text: function(trigger) {
+      return trigger.getAttribute('data-clipboard-text');
+    }
+  });
+
 });
 
 function invertHex(hex) {
@@ -60,12 +85,45 @@ function invertHex(hex) {
 
 function send() {
   var url = $('#addressField').val();
-  console.log(url)
+  if(url == '') return false;
   $.post('http://localhost:9000', {url: url}).done(function(data) {
     console.log(data);
-    $('#code').css({'display': 'block'});
-    $('#code a').attr('href', data.code);
-    $('#code a').text(data.code);
+    $('#code').text(base_url + data.code).attr('data-clipboard-text', base_url + data.code);
   });
 }
 
+
+var colors = [
+  'EF5350',
+  'EC407A',
+  'AB47BC',
+  '7E57C2',
+  '5C6BC0',
+  '42A5F5',
+  '29B6F6',
+  '26C6DA',
+  '26A69A',
+  '66BB6A',
+  '9CCC65',
+  'D4E157',
+  'FFEE58',
+  'FFCA28',
+  'FFA726',
+  'FF7043',
+  '8D6E63',
+  'BDBDBD',
+  '78909C'
+];
+
+var descriptions = [
+  'like dipping it in cold water',
+  'yes. significant shrinkage.',
+  'size does matter.',
+  'same same, but different.',
+  'pint sized.',
+  'mini me.',
+  'bite sized.',
+  'save on bytes.',
+  'try it on your friends!',
+  'hunny, I shrunk the url.'
+];
